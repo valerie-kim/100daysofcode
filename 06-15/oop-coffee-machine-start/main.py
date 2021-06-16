9fd1e906486e8f756273a66a5ad3f0e214e17810
valerie-kim <havennkim@gmail.com>
2021-06-15 23:11:20 -0700

    Commit

--- 06-15/oop-coffee-machine-start/main.py
+++ 06-15/oop-coffee-machine-start/main.py
@@ -1,3 +1,31 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+off = False
+while not off:
+    options = menu.get_items()
+    choice = input(f"What do you want to have? ({options}): ")
+    if choice== "report":
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == "off":
+        off = True
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_resources = coffee_maker.is_resource_sufficient
+        is_enough_money = money_machine.make_payment(drink.cost)
+        if is_enough_resources and is_enough_money:
+            coffee_maker.make_coffee(drink)
+
+            
+        
+            
+    
+    
+    
